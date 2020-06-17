fcd62f713a4500fea05c41f74e647f7f82401afb
Rishab Mathur <rishab.mathur@simplisafe.com>
2020-06-16 21:29:05 -0500

    Updates

---
+++ precache-manifest.58671ae86a0b037582cfb0ad3bdb95a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bfbc2bdcfc38e26ab0d2bfc2ca37e3",
+    "url": "/rishabmathur/index.html"
+  },
+  {
+    "revision": "683f38f35b4162d1a059",
+    "url": "/rishabmathur/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36ac5b496023c3e4b2c2",
+    "url": "/rishabmathur/static/js/2.278b91a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rishabmathur/static/js/2.278b91a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683f38f35b4162d1a059",
+    "url": "/rishabmathur/static/js/main.49e1a029.chunk.js"
+  },
+  {
+    "revision": "cd56c5f3e158f584c767",
+    "url": "/rishabmathur/static/js/runtime-main.61d1cd7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rishabmathur/static/media/logo.5d5d9eef.svg"
+  }
+]);
