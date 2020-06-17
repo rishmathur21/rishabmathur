1b1e33631cd09b1925584dfbd4d48e46c6c869ee
Rishab Mathur <rishab.mathur@simplisafe.com>
2020-06-16 21:28:29 -0500

    deployment packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home></Home>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
